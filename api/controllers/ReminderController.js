0948a9115f2bf7e77d111e011145a70c1de28fd0
Sondre Widmark <sondre.widmark@gmail.com>
2016-02-25 13:14:05 +0100

    Created Reminder API, started filling it. Some syntax fix in User.js

---
+++ api/controllers/ReminderController.js
@@ -0,0 +1,11 @@
+/**
+ * ReminderController
+ *
+ * @description :: Server-side logic for managing reminders
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ api/models/Reminder.js
@@ -0,0 +1,32 @@
+/**
+ * Reminder.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  	attributes: {
+  		id: {
+  			type: 'string',
+  			primaryKey: true,
+  			unique: true
+  		},
+  		ownerID: {
+  			collection: 'user',
+  			via: 'owner'
+  		},
+  		text: {
+  			type: 'string'
+  		},
+  		active: {
+  			type: 'boolean'
+  		}, 
+	  	medicationID: {
+	  		model: 'Medication'
+	  	}
+
+  }
+};
+
