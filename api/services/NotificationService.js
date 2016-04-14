92d14f63b4b7dadcb5b3f962cedd389c93b8eb7b
Eirik Vikanes <eirik.vikanes@gmail.com>
2016-04-14 13:56:53 +0200

    "Improved on the notification functionality, as well as some validation of the types"

--- api/services/NotificationService.js
+++ api/services/NotificationService.js
@@ -2,25 +2,33 @@ var gcm = require("node-gcm");
 
 module.exports = {
 
-	sendTest: function() {
+	notificationTypes: ['remindersChanged', 'medicationsChanged', 'onlineCheck'],
 
-		var apiKey = "AIzaSyARyD62D3oy-1xR4R1PiL1DiX9q-ykfqz0"
-		var token = "cVRin0IbVjw:APA91bEgO-6NaW32xai73-YZXCdDMI9EAn0ZmEC4dBmzNEWgJakgEC2iUzc-I8J8wVLhDL6Q5K_eLi-ZjjScZSkaB_H7oA0QWB_WtO7PMd54N7smvJbIBSP1LiTB_TXgkw_FHLCmRiTX";
+	sendNotification: function(type, token) {
 
-		var message = new gcm.Message(/*{
-		    /*priority: 'high',
+		if (NotificationService.notificationTypes.indexOf(type) < 0) {
+			return sails.log.error('Type has to be a property of NotificationService.notificationTypes.')
+		}
+
+		var sender = new gcm.Sender(sails.config.apiKey);
+		var message = new gcm.Message({
+		    priority: 'high',
+		    contentAvailable: true,
+		    delayWhileIdle: true,
+
 		    notification: {
-		        title: "Hello, World",
-		        icon: "ic_launcher",
-		        body: "This is a notification that will be displayed ASAP."
+		        title: 	"Updates are available",
+		        icon: 	"android_pill",
+		        body: 	"There are updates to your data."
+		    },
+		    data: {
+		    	notificationType: type
 		    }
-		}*/);
+		});
 
-		var sender = new gcm.Sender(apiKey);
-
-		sender.sendNoRetry(message, {to: token}, function(err, response) {
+		sender.send(message, {to: token}, function(err, response) {
 			if (err) 	sails.log.error("Error with gcm: ", err);
-			else 		console.log(response);
+			else 		sails.log.debug(response);
 		});
 	}
 }
