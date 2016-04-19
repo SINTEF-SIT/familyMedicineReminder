dc6c585a0f67fc0c24d700517e4b81a11b8b8884
Sondre Widmark <sondre.widmark@gmail.com>
2016-04-19 13:36:02 +0200

    Comment in package.json removed due to creating issues. AuthController generated

---
+++ api/controllers/AuthControllerController.js
@@ -0,0 +1,11 @@
+/**
+ * AuthControllerController
+ *
+ * @description :: Server-side logic for managing Authcontrollers
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
