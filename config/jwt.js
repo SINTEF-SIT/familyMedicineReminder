41aa2fc6d83b86eae1f9dcec347adca7653b06b8
Sondre Widmark <sondre.widmark@gmail.com>
2016-04-25 19:30:47 +0200

    Created condig file for JWT, to handle global JTW-settings

---
+++ config/jwt.js
@@ -0,0 +1,36 @@
+
+// module.exports.waterlock
+module.exports.waterlock = {
+
+    // CHANGE THIS SECRET BEFORE PRODUCTION
+    secret: '#MyCyFAppTokenPassword007',
+    expiry:{
+      unit: 'days',
+      length: '600'
+    },
+    audience: 'app name',
+    subject: 'subject',
+
+    // tracks jwt usage if set to true - Unlikely to work now
+    trackUsage: true,
+
+    // if set to false will authenticate the
+    // express session object and attach the
+    // user to it during the hasJsonWebToken
+    // middleware - Unlikely to work now
+    stateless: false,
+
+    // set the name of the jwt token property
+    // in the JSON response
+    tokenProperty: 'token',
+
+    // set the name of the expires property
+    // in the JSON response
+    expiresProperty: 'expires',
+
+    // configure whether or not to include
+    // the user in the respnse - this is useful if
+    // JWT is the default response for succesfull login
+    includeUserInJwtResponse: false
+  },
+};
