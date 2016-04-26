911c93c3a403fef502af01dc0815826d1dd9a084
Sondre Widmark <sondre.widmark@gmail.com>
2016-04-26 14:39:40 +0200

    Password generating-function created in UserService

--- api/services/UserService.js
+++ api/services/UserService.js
@@ -1,4 +1,5 @@
 var shortid = require('shortid');
+var crypt = require('crypto');
 
 module.exports = {
 
@@ -6,6 +7,15 @@ module.exports = {
 		do id = shortid.generate().slice(0,5);
 		while (! User.find({ userID: id }));
 		return id;
+	},
+
+	generateRandomHexSequence: function(cb) {
+		crypt.randomBytes(10, function(err, buffer) {
+  			var password = buffer.toString('hex');
+  			sails.log('UserServices-generated password: ', password);
+  			cb(password);
+  			return;
+		});
 	},
 
 	validateUserPassword: function(plainTextPassword, hashedPassword){
