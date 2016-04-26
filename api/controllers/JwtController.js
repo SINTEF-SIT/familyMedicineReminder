e51ddde2992bef92ea799fbeadc1be4a297e25e5
Sondre Widmark <sondre.widmark@gmail.com>
2016-04-26 19:50:14 +0200

    Generated controller for Jwt

---
+++ api/controllers/JwtController.js
@@ -0,0 +1,11 @@
+/**
+ * JwtController
+ *
+ * @description :: Server-side logic for managing Jwts
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
