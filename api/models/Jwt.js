b10726877a0bceebcd186add6c604f0efd618c10
Sondre Widmark <sondre.widmark@gmail.com>
2016-04-29 20:21:11 +0200

    Formating changes in Jwt model and just-in-case function to check for token == null

--- api/models/Jwt.js
+++ api/models/Jwt.js
@@ -7,24 +7,36 @@
 
 module.exports = {
 
-  attributes: {
+    attributes: {
 
-    token: {
-    	type: 'string',
-    	primaryKey: true,	
-  		unique: true
-    },
-	expiry: {
-	  	type: 'string'
-	},
-	owner: {
-		model: 'user',
-		required: true
-	},
-	revoked: {
-	  	type: 'boolean',
-	  	defaultsTo: false 
+	    token: {
+	    	type: 'string',
+	    	primaryKey: true,	
+	  		unique: true
+	    },
+		expiry: {
+		  	type: 'string'
+		},
+		owner: {
+			model: 'user',
+			required: true
+		},
+		revoked: {
+		  	type: 'boolean',
+		  	defaultsTo: false 
+		}
+  	} //,
+
+  	// Should not be possible anyway, but this checks that token is not null
+  	/*
+  	beforeCreate: function(jwt, cb) {
+		if (jwt.token === null) {
+			jwt.token = 'Something wrong happend when generating JWT';
+			sail.log.error('User',jwt.owner,'has token null. Something went wrong in generating JWT');
+		} 
+
+		cb(null, jwt);	
 	}
-  }
+	*/
 };
 
