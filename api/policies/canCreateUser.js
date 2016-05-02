b2b3fd7992b317f266ed4088f70168f375689a77
Sondre Widmark <sondre.widmark@gmail.com>
2016-05-02 19:40:11 +0200

    Allowing admin to create user without create secret

--- api/policies/canCreateUser.js
+++ api/policies/canCreateUser.js
@@ -2,6 +2,12 @@
 
 module.exports = function(req, res, next) {
 
+	 // If user has token with admin privileges, skip all authentification.
+	if (JwtService.hasFullAccess(req.headers.access_token)) {
+    	sails.log("Admin granted access to "+req.originalUrl);
+		return next();
+    }
+
 	var systemCreateSecret = sails.config.globals.createSecret;
 	var userCreateSecret = req.headers.create_secret;
 	var originalUrl = req.originalUrl;
