9d7f206a1d5b8d480bcebbbcd75e269999b2951d
Sondre Widmark <sondre.widmark@gmail.com>
2016-05-04 18:13:29 +0200

    Created function for returning the whole user object

--- api/services/UserService.js
+++ api/services/UserService.js
@@ -19,6 +19,70 @@ module.exports = {
 		});
 	},
 
+	returnUserObject: function(userID, cb){
+
+		// sails.log('returnUserObject()');
+		// sails.log('userID:',userID);
+		User.findOne({userID: userID})
+		.populate(jsonWebToken)
+		.populate(guardian)
+		.populate(child)
+		.then(function(user){
+			if (typeof user === 'undefined') {
+				return Promise.reject("Cannot return user object of 'undefined'");
+			} //sails.log("User:",user);
+			cb(user);
+			return Promise.resolve();
+
+		})
+		.catch(function(err){
+			//return false;
+			return this.res.failure("Error in UserService.returnUserObject():",err);
+		})
+	},
+
+
+
+	returnChildsGuardians: function(childID){
+		UserService.returnUserObject(childID, function (child){
+			//sails.log('callback function done');
+			sails.log('Child:\n',child);
+		});
+		
+	/*
+		addChild: function(req, res) {
+		var userID = req.param('userID');
+		var childID = req.body.childID;
+
+		// Check if childID is defined in HTTP body
+		if (typeof childID === 'undefined') 
+			return res.failure('\'childID\' is not defined in HTTP body. Cannot add child \'undefined\'');
+
+		User.findOne({ userID: userID })
+		.populate('children')
+		.then(function(user) {
+			if (user.children.every(child => child.userID !== childID)) {
+				return Promise.resolve(user);
+			}
+			return Promise.reject("User is already a child");
+		})
+		.then(function(user) {
+			user.children.add(childID);
+			user.save(function(err, user) {
+				if (err) 	return Promise.reject(err);
+				sails.log.debug(user);
+			});
+			res.send({"message": "Child was added"});
+			sails.log.debug(userID, "added", childID, "as a child");
+			return Promise.resolve();
+		})
+		.catch(function(err) {
+			sails.log.error("Could not add child: ", err);
+			return res.send({"message" : err});
+		});*/
+
+	},
+
 	validateUserPassword: function(plainTextPassword, hashedPassword){
 
 	}
