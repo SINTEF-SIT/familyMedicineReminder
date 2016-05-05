22abb3b4e54b82b85a0f9e56042adb717c581ae7
Sondre Widmark <sondre.widmark@gmail.com>
2016-05-05 18:54:51 +0200

    Created several small functions to serve the purpose of notifying guardian when child made changes

--- api/services/NotificationService.js
+++ api/services/NotificationService.js
@@ -37,5 +37,23 @@ module.exports = {
 			if (err) 	sails.log.error("Error with gcm: ", err);
 			else 		sails.log.debug(response);
 		});
-	}
+	},
+
+	notifyGuardiansOfChange: function(childID){
+		UserService.returnGuardianTokens(childID, function(tokens){
+			// 'tokens' are, if guardian with tokens exist, on the format: [[guardianID], [token]]
+			// If user has no guardian and/or no token, 'tokens' is simply 'false'
+			
+			// sails.log('notifyGuardiansOfChange tokens:', tokens);
+			if (tokens) {
+				var data = 'Your child '+childID+' has made changes';
+				for (var i = 0; i < tokens[1].length; i++){
+					sails.log("Sending notification to ",tokens[0][i]);
+					NotificationService.sendNotification('medicationsChanged', data, tokens[1][i])
+				} sails.log('Notifications sent to guardian(s) of',childID);
+			} else {
+				sails.log('User has no guardians with token');
+			}
+		})
+	},
 }
