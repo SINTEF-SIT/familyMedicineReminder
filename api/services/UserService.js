e6691a2b6dfbd62a1f8b25886e79c4d1e1cd7bcf
Sondre Widmark <sondre.widmark@gmail.com>
2016-05-08 15:21:53 +0200

    Fixed merge conflict

--- api/services/UserService.js
+++ api/services/UserService.js
@@ -30,12 +30,8 @@ module.exports = {
 	},
 
 	returnUserObject: function(userID, attribute/*, cb*/){
-<<<<<<< HEAD
 		sails.log.info('returnUserObject()');
 		return User.findOne({userID: userID})
-=======
-		User.findOne({userID: userID})
->>>>>>> 80f892ff0a0cc467767b27b91d79d0f9bee5eb17
 		.populate(attribute)
 		.then((user) => {
 			// sails.log('returnUserObject user:',user);
@@ -44,11 +40,7 @@ module.exports = {
 			} /*if (typeof user.token === 'undefined' || user.token === null) {
 				return Promise.reject('Users guardian(s) does not have gcm token');
 			}*/
-<<<<<<< HEAD
 			sails.log("User from UserService.returnUserObject():\n",user);
-=======
-			// sails.log("User from UserService.returnUserObject():\n",user);
->>>>>>> 80f892ff0a0cc467767b27b91d79d0f9bee5eb17
 			return Promise.resolve(user);
 			//return Promise.resolve();
 
@@ -61,55 +53,35 @@ module.exports = {
 		});
 	},
 
-<<<<<<< HEAD
 	returnFullUserObject: function(userID){
 		sails.log.info('returnFullUserObject()');
 		// sails.log('returnUserObject()');
 		// sails.log('userID:',userID);
 		return User.findOne({userID: userID})
-		.populate('guardians')
-=======
-	returnFullUserObject: function(userID, cb){
-		// sails.log('returnUserObject()');
-		// sails.log('userID:',userID);
-		User.findOne({userID: userID})
 		.populate('jsonWebToken')
 		.populate('guardians')
 		.populate('children')
 		.populate('medications')
 		.populate('reminders')
->>>>>>> 80f892ff0a0cc467767b27b91d79d0f9bee5eb17
 		.then(function(user){
 			if (typeof user === 'undefined' ) {
 				return Promise.reject("Cannot return user object of 'undefined'");
 			} 
-<<<<<<< HEAD
 			sails.log("User from UserService.returnFullUserObject:",user);
 			//return cb(user);
 			//return Promise.resolve();
 			return Promise.resolve(user);
-=======
-			//sails.log("User:",user);
-			return cb(user);
-			//return Promise.resolve();
->>>>>>> 80f892ff0a0cc467767b27b91d79d0f9bee5eb17
 
 		})
 		.catch(function(err){
 			sails.log.error('Error in UserService.returnFullUserObject:',err);
 			// Returns error - res not defined, can't reach it with 'this'. not important, just log
 			// return this.res.failure("Error in UserService.returnFullUserObject():",err);
-<<<<<<< HEAD
 			return false;
-=======
-			return;
->>>>>>> 80f892ff0a0cc467767b27b91d79d0f9bee5eb17
 		});
 	},
 
 	returnIdListOfAttributeInObject: function(json, attribute, id){
-<<<<<<< HEAD
-
 		// Redundant?
 
 		var attributeList = json[attribute];
@@ -172,79 +144,6 @@ module.exports = {
 			sails.log(err);
 		});
 	},
-=======
-
-		// Redundant?
-
-		var attributeList = json[attribute];
-		var returnList = [];
-		// sails.log('attributeList['+attribute+']:',attributeList);
-
-		if (typeof attributeList.id === 'undefined') {
-			sails.log('User',json.userID,'doesn\'t have an attribute user.'+attribute+'.'+id);
-			return false;
-		} if (typeof attributeList === 'undefined' || attributeList.length === 0) {
-			sails.log('User',json.userID,'doesn\'t have an object for user.'+attribute);
-			return false;
-		}
-		//if (attributeList.length == 1) return attributeList[0][id];
-
-		for (var i = 0; i < attributeList.length; i++){
-			// sails.log('attributeList[i][id]:',attributeList[i][id]); 
-			returnList.push(attributeList[i][id]);
-		} return returnList;
-
-	},
-
-	returnGuardianTokenData: function(childID/*, cb*/){
-		UserService.returnUserObject(childID, 'guardians') 
-		.then(function (child){
-			//sails.log('returnGuardianTokenData child:',child);
-			var guardians = child.guardians;
-			var compiledList = [[],[],[]];
-			for (var i = 0; i < guardians.length; i++) {
-				// debug
-				sails.log('compiledList['+i+']:')
-				sails.log('userID:',guardians[i]['userID']);
-				sails.log('receiveChangeNotification:', guardians[i]['receiveChangeNotification']);
-				sails.log('token:',guardians[i]['token']);
-				// Validate guardian wants and is able to receive push notification about change
-				if (typeof guardians[i]['token'] === 'undefined' || guardians[i]['token'] === null) {
-					sails.log(childID+"'s guardian "+guardians[i]['userID']+" doesn't have a defined gcm token");
-				} else if (!guardians[i]['receiveChangeNotification']) {
-					sails.log(childID+"'s guardian "+guardians[i]['userID']+" has receiveChangeNotification: false");
-				} else { //  if (receiveChangeNotification && token != null) 
-					compiledList[0].push(guardians[i]['userID']);
-					compiledList[1].push(guardians[i]['receiveChangeNotification']);
-					compiledList[2].push(guardians[i]['token']);
-				}
-			}
-			if (compiledList[0].length > 0) {
-				sails.log('UserService.returnGuardianTokenData() compiledList:\n',compiledList);
-				return Promise.resolve(compiledList);
-				//return Promise.resolve(compiledList);
-			} else {
-				sails.log('User',childID,'doesn\'t have guardians');
-				//return false;
-				//return cb(false);
-				return Promise.reject('User',childID,'doesn\'t have guardians');
-			}
-		})
-		.catch((err) => {
-			sails.log(err);
-		});
-	},
-
-
-
-
-
-
-
-
-
-
->>>>>>> 80f892ff0a0cc467767b27b91d79d0f9bee5eb17
 
 	returnGuardianTokens: function(childID, cb) {
 		UserService.returnGuardianTokenData(childID, function(guardianList){
